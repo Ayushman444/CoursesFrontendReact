00c502481a2e55b4e58e57f352a2231c3a003e07
Ayushman Tiwari <ayushman5022@gmail.com>
2024-01-10 21:26:56 +0530

    formatting

--- src/App.js
+++ src/App.js
@@ -1,60 +1,56 @@
 import React, { useEffect, useState } from "react";
 import { Navbar } from "./components/Navbar.js";
 import { Cards } from "./components/Cards.js";
-import {Filter} from "./components/Filter.js"
-import {apiUrl,filterData} from "./data"
-import { toast } from 'react-toastify';
-import 'react-toastify/dist/ReactToastify.css';
+import { Filter } from "./components/Filter.js";
+import { apiUrl, filterData } from "./data";
+import { toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 import { Spinner } from "./components/Spinner.js";
 
-
 const App = () => {
+  const [courses, setCourses] = useState(null);
+  const [loading, setLoading] = useState(true);
+  const [category, setCategory] = useState(filterData[0].title);
 
-  const [courses,setCourses] = useState(null);
-  const [loading,setLoading] = useState(true);
-  const [category,setCategory] = useState(filterData[0].title)
-
-  async function fetchData(){
+  async function fetchData() {
     setLoading(true);
-    try{
+    try {
       let response = await fetch(apiUrl);
       let output = await response.json();
 
       setCourses(output.data);
-
-    }
-    catch(error){
+    } catch (error) {
       toast.error("Network me koi dikkat hai");
     }
     setLoading(false);
   }
 
-  useEffect(()=>{
+  useEffect(() => {
     fetchData();
-  },[])
+  }, []);
   return (
     <div className="flex flex-col min-h-screen bg-bgDark2">
-
       <div>
         <div>
-          <Navbar/>
+          <Navbar />
         </div>
 
         <div>
-          <Filter filterData={filterData}
-          setCategory={setCategory}
-          category={category}/>       
+          <Filter
+            filterData={filterData}
+            setCategory={setCategory}
+            category={category}
+          />
         </div>
 
         <div className="w-11/12 max-w-[1200px] mx-auto flex flex-wrap justify-center items-center mih-h-[50h]">
-          {
-            loading? (<Spinner/>) : (<Cards courses={courses} category={category}/>)
-          }
+          {loading ? (
+            <Spinner />
+          ) : (
+            <Cards courses={courses} category={category} />
+          )}
         </div>
       </div>
-
-
-
     </div>
   );
 };
--- src/components/Spinner.js
+++ src/components/Spinner.js
@@ -1,13 +1,11 @@
-import React from 'react'
-import "./Spinner.css"
+import React from "react";
+import "./Spinner.css";
 
 export const Spinner = () => {
   return (
-    <div className='flex flex-col items-center space-y-2'>
-        <span className='text-bgDark text-lg font-semibold'>Loading...</span>
-        <div className='dots'></div>
-        
-        
+    <div className="flex flex-col items-center space-y-2">
+      <span className="text-bgDark text-lg font-semibold">Loading...</span>
+      <div className="dots"></div>
     </div>
-  )
-}
+  );
+};
